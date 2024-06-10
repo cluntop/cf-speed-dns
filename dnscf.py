b2a4012c003363357340566f086b498f101cbf9b
雨呢 <85211716+cluntop@users.noreply.github.com>
2024-06-10 16:56:46 +0800

    Update dnscf.py

--- dnscf.py
+++ dnscf.py
@@ -31,27 +31,27 @@ def get_cf_speed_test_ip(timeout=10, max_retries=5):
                 return response.text
         except Exception as e:
             traceback.print_exc()
-            print(f"get_cf_speed_test_ip Request failed (attempt {attempt + 1}/{max_retries}): {e}")
+            print("get_cf_speed_test_ip Request failed (attempt {attempt + 1}/{max_retries}): {e}")
     # 如果所有尝试都失败，返回 None 或者抛出异常，根据需要进行处理
     return None
 
 # 获取 DNS 记录
 def get_dns_records(name):
     def_info = []
-    url = f'https://api.cloudflare.com/client/v4/zones/{CF_ZONE_ID}/dns_records'
+    url = 'https://api.cloudflare.com/client/v4/zones/{CF_ZONE_ID}/dns_records'
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         records = response.json()['result']
         for record in records:
             if record['name'] == name:
-                def_info.append(record['dns_record_id'])
+                def_info.append(record['id'])
         return def_info
     else:
         print('Error fetching DNS records:', response.text)
 
 # 更新 DNS 记录
-def update_dns_record(record_id, name, cf_ip):
-    url = f'https://api.cloudflare.com/client/v4/zones/{CF_ZONE_ID}/dns_records/{dns_record_id}'
+def update_dns_record(dns_record_id, name, cf_ip):
+    url = 'https://api.cloudflare.com/client/v4/zones/{CF_ZONE_ID}/dns_records/{dns_record_id}'
     data = {
         'type': 'A',
         'name': name,
@@ -89,12 +89,12 @@ def main():
     # 获取最新优选IP
     ip_addresses_str = get_cf_speed_test_ip()
     ip_addresses = ip_addresses_str.split(',')
-    dns_record_id = get_dns_records(CF_DNS_NAME)
+    dns_records = get_dns_records(CF_DNS_NAME)
     push_plus_content = []
     # 遍历 IP 地址列表
     for index, ip_address in enumerate(ip_addresses):
         # 执行 DNS 变更
-        dns = update_dns_record(dns_record_id[index], CF_DNS_NAME, ip_address)
+        dns = update_dns_record(dns_records[index], CF_DNS_NAME, ip_address)
         push_plus_content.append(dns)
 
     push_plus('\n'.join(push_plus_content))
