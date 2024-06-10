a3a833bcc12d76c48b9b09e7aeb084c01d586ebc
雨呢 <85211716+cluntop@users.noreply.github.com>
2024-06-10 15:49:26 +0800

    Update dnscf.py

--- dnscf.py
+++ dnscf.py
@@ -15,7 +15,7 @@ PUSHPLUS_TOKEN  =   os.environ["PUSHPLUS_TOKEN"]
 
 
 headers = {
-    'Authorization': f'Bearer {CF_API_TOKEN}',
+    'X-Auth-Email': f'Bearer {CF_API_TOKEN}',
     'Content-Type': 'application/json'
 }
 
