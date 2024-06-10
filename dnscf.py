528071df00156758b0654d478a66fd030ab45526
雨呢 <85211716+cluntop@users.noreply.github.com>
2024-06-10 16:34:14 +0800

    Update dnscf.py

--- dnscf.py
+++ dnscf.py
@@ -89,12 +89,12 @@ def main():
     # 获取最新优选IP
     ip_addresses_str = get_cf_speed_test_ip()
     ip_addresses = ip_addresses_str.split(',')
-    dns_records = get_dns_records(CF_DNS_NAME)
+    dns_record_id = get_dns_records(CF_DNS_NAME)
     push_plus_content = []
     # 遍历 IP 地址列表
     for index, ip_address in enumerate(ip_addresses):
         # 执行 DNS 变更
-        dns = update_dns_record(dns_records[index], CF_DNS_NAME, ip_address)
+        dns = update_dns_record(dns_record_id[index], CF_DNS_NAME, ip_address)
         push_plus_content.append(dns)
 
     push_plus('\n'.join(push_plus_content))
